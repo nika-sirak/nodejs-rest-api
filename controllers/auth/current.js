61a3ce414136fd2e7978d2795a599fee43277565
Veronika Sirak <7nicka@gmail.com>
2022-09-06 23:59:14 +0100

    fix bug

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,13 +1,5 @@
-const { User } = require("../../models/user");
-const { RequestError } = require("../../helpers");
-
 const current = async (req, res) => {
-  const { _id, email, subscription } = req.user;
-
-  const user = await User.find({ _id });
-  if (!user) {
-    throw RequestError(401, "Not authorized");
-  }
+  const { email, subscription } = req.user;
 
   res.json({
     email,
